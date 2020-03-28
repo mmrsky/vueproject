dae00a1e7c233243bf5acee161cc5721f158d475
mika <mika.morsky@gmail.com>
2020-03-28 20:02:32 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Matkalasku logo" src="./assets/logo2.png">
+    <Matkalasku msg="Matkalasku sovellus"/>
+  </div>
+</template>
+
+<script>
+import Matkalasku from './components/Matkalasku.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Matkalasku
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Matkalasku.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="matkalasku">
+    <!-- <h1>{{ msg }}</h1> -->
+    <h1>Matkalaskutin</h1>
+    <h4>Anna matkan alkupäivä</h4>
+    <input type="text" name=""> <!-- value="" v-model="newItem" @keyup.enter="addToList()">-->
+    <h4>Anna matkan loppuaika</h4>
+    <input type="text" name=""> <!-- value="" v-model="newItem" @keyup.enter="addToList()">-->
+    <h4>Anna matkan kohde</h4>
+    <input type="text" name=""> <!-- value="" v-model="newItem" @keyup.enter="addToList()">-->    
+    <h4>Anna matkan aihe</h4>
+    <input type="text" name=""> <!-- value="" v-model="newItem" @keyup.enter="addToList()">-->    
+    <br><button>Lähetä</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Matkalasku',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
