47191c18181415d8f7412b567c2c52424f80a243
mika <mika.morsky@gmail.com>
2020-03-31 23:52:36 +0300

    .....

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // server.jsvar express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');app = express();
+import path from 'path';
+import serveStatic from 'serve-static';app = express();
 app.use(serveStatic(__dirname + "/dist"));var port = process.env.PORT || 5000;
 app.listen(port);console.log('server started '+ port);
